3bf4cefae1e6e80df3cca9a90f5a223259989e8d
usilva <usilva@a5solutions.com>
2019-07-12 14:34:05 -0300

    configuracao dos controllers e routers

--- multiroomchat/app/routes/index.js
+++ multiroomchat/app/routes/index.js
@@ -1,7 +1,6 @@
 module.exports = function(application){
-
-    application.get('/', function(req, res){
-        res.render('index');
+application.get('/', function(req, res){
+        application.app.controllers.index.home(application, req, res);
     });
-
+    
 }
