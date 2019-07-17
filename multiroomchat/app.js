1cc5544014b0f8cb4112fdc296de1672e15e4d86
usilva <usilva@a5solutions.com>
2019-07-17 17:19:59 -0300

    utilizacao dos metodos io e emit do websocket

--- multiroomchat/app.js
+++ multiroomchat/app.js
@@ -2,6 +2,19 @@
 var app = require('./config/server');
 
 /* parametrizar porta de escuta */
-app.listen(80, function(){
+var server = app.listen(80, function(){
     console.log('Servidor online');
 })
+
+var io = require('socket.io').listen(server);
+
+// var global
+app.set('io',io);
+/* criar conexão por websocket */
+io.on('connection',function(socket){
+	console.log('Usuário conectou');
+
+	socket.on('disconnect',function(){
+		console.log('Usuário desconectou');
+	});
+});
