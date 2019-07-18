5df88f2810fa19013fc5dcda34dfa85f067d945d
usilva <usilva@a5solutions.com>
2019-07-18 16:29:32 -0300

    fim do projeto multiroomchat

--- multiroomchat/app.js
+++ multiroomchat/app.js
@@ -14,7 +14,34 @@ app.set('io',io);
 io.on('connection',function(socket){
 	console.log('Usuário conectou');
 
+		// socket.on - ouve mensagem
 	socket.on('disconnect',function(){
 		console.log('Usuário desconectou');
 	});
+
+	socket.on('msgParaServidor', function(data){
+		// socket.emit envia mensagem
+
+		/* dialogos */
+		socket.emit('msgParaCliente',
+		{apelido : data.apelido, mensagem: data.mensagem}
+		);
+
+		// broadcast - faz o que a mensagem enviada pelo servidor, seja replicada para todos os clientes (views do chat)
+		socket.broadcast.emit('msgParaCliente',
+		{apelido: data.apelido, mensagem: data.mensagem}
+		);
+
+		/* Participantes */
+
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+			socket.emit('participantesParaClientes',
+			{apelido : data.apelido}
+			);
+
+			socket.broadcast.emit('participantesParaClientes',
+			{apelido: data.apelido}
+			);
+		}		
+});
 });
--- multiroomchat/config/server.js
+++ multiroomchat/config/server.js
@@ -28,8 +28,7 @@ app.use(expressValidator());
 
 /* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
 consign()
-    .include('app/routes')
-    .then('app/models')
+.include('app/routes')
     .then('app/controllers')
     .into(app);
 
